29df280c21bcd1b5a1578ced166147752a16514d
Aman-code597 <amancpanand@gmail.com>
2022-09-27 15:30:48 +0530

    New dll added

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Ems;
 
 namespace HelloWorld
 {
@@ -10,8 +11,12 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
+
             Console.WriteLine("Hello world");
             Console.WriteLine("Hey cg");
+            Emp emp = new Emp { Id = 20, Name = "Tom", Salary = 4000 };
+            Console.WriteLine($"{emp.Id} {emp.Name} {emp.Salary}");
+
         }
     }
 }
