c8650283453622031bd99f67d6b3157da4a558e4
Aman-code597 <amancpanand@gmail.com>
2022-09-27 15:19:00 +0530

    File added

---
+++ HelloWorld/Products.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Products
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Rate { get; set; }
+    }
+}
